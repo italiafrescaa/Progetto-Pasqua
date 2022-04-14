f600befe2abbfd50553c25ea23a46779bd4503a4
michele coffele <8293449@itisrossi.vi.it>
2022-04-14 16:21:54 +0100

    Aggiungere i file di progetto.

---
+++ 07.04.2023_Progetto_Pasqua/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07._04._2023_Progetto_Pasqua
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
